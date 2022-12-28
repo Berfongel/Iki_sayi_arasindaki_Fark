3f72897e26989b4d30c385b22e26c57e8dc7b4a5
Berfongel <121492984+Berfongel@users.noreply.github.com>
2022-12-28 16:04:37 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+int sayi1;
+int sayi2;
+
+Console.WriteLine("Birinci sayıyı giriniz :");
+sayi1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("İkinci sayıyı giriniz :");
+sayi2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Fark : " + (sayi1-sayi2));
