862f75b060a8e4af59984e7008fb74b26cdd32b3
Berfongel <121492984+Berfongel@users.noreply.github.com>
2023-01-04 17:11:41 +0300

    Add files via upload

---
+++ Fark.cs
@@ -0,0 +1,8 @@
+﻿namespace FarkWebApplication.Models
+{
+    public class Fark
+    {
+        public double BirinciSayi { get; set; }
+        public double İkinciSayi { get; set; }
+    }
+}
---
+++ HomeController.cs
@@ -0,0 +1,38 @@
+﻿using FarkWebApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace FarkWebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index(Fark fark)
+        {
+            var number1_ = fark.BirinciSayi;
+            var number2_ = fark.İkinciSayi;
+            var islem = (number1_ - number2_);
+            var message = string.Empty;
+            message = "Fark : " + islem;
+            ViewBag.Message = message;
+            return View("fark");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
